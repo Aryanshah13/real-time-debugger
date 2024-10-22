4e5065b66f5f20ccad8905a21d2147c007555e11
Aryanshah13 <shaharyand@gmail.com>
2024-10-22 22:26:20 +0530

    updated code

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,6 +8,7 @@ const Home = () => {
 
     const [roomId, setRoomId] = useState('');
     const [username, setUsername] = useState('');
+
     const createNewRoom = (e) => {
         e.preventDefault();
         const id = uuidV4();
@@ -34,61 +35,57 @@ const Home = () => {
             joinRoom();
         }
     };
-  return (
-    <div className='homePageWrapper'>
-      <div className='formWrapper'>
-        <img
-          className="homePageLogo"
-          src="/code.png"
-          alt="code-pulse-logo"
-        
-        />
-        <h4 className='mainLabel'>
-          Paste invitation ID
-        </h4>
-        <div className='inputGroup'>
-          <input 
-          type='text' 
-          className='inputBox' 
-          placeholder='Room ID' 
-          onChange={(e)=>setRoomId(e.target.value)} 
-          value={roomId}
-          onKeyUp={handleInputEnter}
-          />
-          
-          <input 
-          type='text' 
-          className='inputBox' 
-          placeholder='USERNAME' 
-          onChange={(e)=>setUsername(e.target.value)} 
-          value={username}
-          onKeyUp={handleInputEnter}
-          />
-          
-          
-          <button  className='btn joinBtn' onClick={joinRoom}>
-            Join
-          </button>
-          <span className='createInfo'>
-            If you do not have an invitation, then create &nbsp;
-            <a 
-              onClick= {createNewRoom} 
-              href='#' 
-              className='createNewBtn'
-            >
-              new Room 
-            </a>
-          </span>
+
+    return (
+        <div className='homePageWrapper'>
+            <div className='formWrapper'>
+                <img
+                    className="homePageLogo"
+                    src="/code.png"
+                    alt="code-pulse-logo"
+                />
+                <h4 className='mainLabel'>Paste invitation ID</h4>
+                <div className='inputGroup'>
+                    <input 
+                        type='text' 
+                        className='inputBox' 
+                        placeholder='Room ID' 
+                        onChange={(e) => setRoomId(e.target.value)} 
+                        value={roomId}
+                        onKeyUp={handleInputEnter}
+                    />
+                    
+                    <input 
+                        type='text' 
+                        className='inputBox' 
+                        placeholder='USERNAME' 
+                        onChange={(e) => setUsername(e.target.value)} 
+                        value={username}
+                        onKeyUp={handleInputEnter}
+                    />
+                    
+                    <button className='btn joinBtn' onClick={joinRoom}>
+                        Join
+                    </button>
+                    <span className='createInfo'>
+                        If you do not have an invitation, then create &nbsp;
+                        <button 
+                            onClick={createNewRoom} 
+                            className='createNewBtn'
+                        >
+                            new Room 
+                        </button>
+                    </span>
+                </div>
+            </div>
+            <footer>
+                <h4>
+                    Built by 💛 &nbsp; by &nbsp;
+                    <a href='https://github.com/Dhirajsharma2060'>Aryan@2060</a>
+                </h4>
+            </footer>
         </div>
-      </div>
-      <footer>
-        <h4>
-          Built by 💛 &nbsp; by &nbsp;
-          <a href='https://github.com/Dhirajsharma2060'>Aryan@2060</a>
-        </h4>
-      </footer>
-    </div>
-  );
+    );
 };
 
 export default Home;
